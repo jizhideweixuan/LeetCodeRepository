efa82a1f254e14a4d4d695808396dcb27259271f
jizhideweixuan <t467902051@outlook.com>
2019-02-28 15:15:34 +0800

    增加了cin/cout加速机制

--- 14_LongestCommonPrefix/main.cpp
+++ 14_LongestCommonPrefix/main.cpp
@@ -2,22 +2,31 @@
 #include <vector>
 using namespace std;
 
+static auto io_sync_off = []() {
+    //不再兼容scanf和printf,让std::cin和std::cout不再经过缓冲区
+    std::ios::sync_with_stdio(false);
+    //解除std::cin和std::cout之间的绑定
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+}();
+
 string longestCommonPrefix(vector<string> &strs) {
     string res = "";
     string world;
     string nextWorld;
     //两个字符串相同开头的长度
     unsigned int preLength;
-    if(strs.empty())
+    if (strs.empty())
         return "";
     else if (strs[0] == "")
         return "";
-    else if(strs.size() == 1)
+    else if (strs.size() == 1)
         return strs[0];
     world = strs[0];
     preLength = strs[0].size();
-    for (int i = 1; i < strs.size(); ++i) {
-        nextWorld = strs[i];
+    for (vector<string>::iterator itr = strs.begin() + 1; itr != strs.end(); itr++) {
+        nextWorld = *itr;
         //相同长度和下一个字符串长度,取较小值
         preLength = min(preLength, (unsigned int) nextWorld.size());
         //找出两个字符串相同开头的长度
@@ -34,5 +43,6 @@ string longestCommonPrefix(vector<string> &strs) {
 int main() {
     vector<string> strs = {"flower", "flow", "flight"};
     cout << longestCommonPrefix(strs) << endl;
+    cout << bestLongestCommonPrefix(strs) << endl;
     return 0;
 }
