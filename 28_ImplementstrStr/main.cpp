fe770038d7e7f5e97da3e686a4b6e2c52bffce9e
jizhideweixuan <t467902051@outlook.com>
2019-03-07 18:24:22 +0800

    KMP

--- 28_ImplementstrStr/main.cpp
+++ 28_ImplementstrStr/main.cpp
@@ -15,23 +15,30 @@ static auto io_sync_off = []() {
 }();
 
 int getStr(string haystack, string needle) {
-    if (needle.empty()) return 0;
+    if (needle.empty())
+        return 0;
 
     //获取next数组
     vector<int> pi(needle.size(), 0);
     for (int i = 1, k = 0; i < needle.size(); i++) {
+        //当k!=0且needle[k] != needle[i]
         while (k && needle[k] != needle[i])
+            // k = next[k-1]
             k = pi[k - 1];
+        //若最后needle[k] == needle[i],next[i] = ++k 否则 next[i] = 0
         if (needle[k] == needle[i])
             k++;
         pi[i] = k;
     }
 
     for (int i = 0, k = 0; i < haystack.size(); ++i) {
+        //字符不同,i++,k=next[k]
         while (k && needle[k] != haystack[i])
             k = pi[k - 1];
+        //字符相同,下一轮k++.i++
         if (needle[k] == haystack[i])
             k++;
+        //k走到头,匹配成功
         if (k == needle.size())
             return i - k + 1;
     }
