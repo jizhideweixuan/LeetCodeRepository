0f4668f3ebdbb3a83d338089e074792ab4c2f5e3
jizhideweixuan <t467902051@outlook.com>
2019-03-11 14:13:46 +0800

    Search Insert Position

---
+++ 35_SearchInsertPosition/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int searchInsert(vector<int> &nums, int target);
+
+static auto io_sync_off = []() {
+    //不再兼容scanf和printf,让std::cin和std::cout不再经过缓冲区
+    std::ios::sync_with_stdio(false);
+    //解除std::cin和std::cout之间的绑定
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+}();
+
+int searchInsert(vector<int> &nums, int target) {
+    if (nums.empty())
+        return 0;
+    int low = 0;
+    int high = nums.size() - 1;
+    int mid;
+    while (low <= high) {
+        mid = (low + high) / 2;
+        if (nums[mid] == target)
+            return mid;
+        else if (nums[mid] > target)
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+    return low;
+}
+
+int main() {
+    vector<int> nums = {1, 3, 5, 6};
+    std::cout << searchInsert(nums, 7) << std::endl;
+    return 0;
+}
