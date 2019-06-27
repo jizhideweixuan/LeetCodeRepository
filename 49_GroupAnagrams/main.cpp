d8e6b2727f13164812eb0bb3abd633036e0992b8
jizhideweixuan <t467902051@outlook.com>
2019-06-27 18:41:05 +0800

    18:41 修改 Signed-off-by: jizhideweixuan <t467902051@outlook.com>

--- 49_GroupAnagrams/main.cpp
+++ 49_GroupAnagrams/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
-#include <map>
+#include <unordered_map>
+#include <algorithm>
 
 using namespace std;
 
@@ -14,29 +15,19 @@ static auto io_sync_off = []() {
 
 vector<vector<string>> groupAnagrams(vector<string> &strs) {
     vector<vector<string>> res;
-    if (strs.size() == 0) {
-        return res;
+    int n = strs.size();
+    vector<string> s = strs;
+    unordered_map<string, int> order_map;
+    for (int i = 0; i < n; ++i) {
+        sort(s[i].begin(), s[i].end());
     }
-    map<string, vector<string>> order_map;
-    for (int i = 0; i < strs.size(); ++i) {
-        int count[26] = {};
-        for (int j = 0; j < strs[i].size(); ++j) {
-            count[strs[i][j] - 'a']++;
+    for (int i = 0; i < n; ++i) {
+        if (order_map.find(s[i]) == order_map.end()) {
+            res.push_back({strs[i]});
+            order_map[s[i]] = res.size() - 1;
+        } else {
+            res[order_map[s[i]]].push_back(strs[i]);
         }
-        string row_sum = "";
-        for (int k = 0; k < 26; ++k) {
-            row_sum += "#"+to_string(count[k]);
-        }
-        vector<string> &v = order_map[row_sum];
-        if (v.size()==0){
-            order_map[row_sum] = vector<string>{strs[i]};
-        } else{
-            v.push_back(strs[i]);
-        }
-    }
-    for (map<string, vector<string>> ::iterator it = order_map.begin(); it != order_map.end(); it++)
-    {
-        res.push_back(order_map[it->first]);
     }
     return res;
 }
