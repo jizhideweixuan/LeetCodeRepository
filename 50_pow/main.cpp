13cfd40d82921b05533de074732e7d94386f76fc
jizhideweixuan <t467902051@outlook.com>
2019-06-28 17:14:15 +0800

    pow
    Signed-off-by: jizhideweixuan <t467902051@outlook.com>

---
+++ 50_pow/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+double myPow(double x, int n);
+
+static auto io_sync_off = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+double myPow(double x, int n) {
+    long long  N = n;
+    if (N < 0) {
+        x = 1 / x;
+        N = -N;
+    }
+    double ans = 1;
+    double current = x;
+    for (long long i = N; i; i /= 2) {
+        if ((i % 2) == 1) {
+            ans = ans * current;
+        }
+        current *= current;
+    }
+    return ans;
+}
+
+int main() {
+    cout << myPow(2.0, 10);
+    return 0;
+}
